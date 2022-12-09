711c92ecd94c258d5cd6e0b6c7756f2eb86e1ff1
Jinyin Chai <cjy990324>
2022-12-08 19:40:08 -0800

    v2

--- routes/auth.js
+++ routes/auth.js
@@ -46,7 +46,7 @@ router.post("/login", async (req, res) => {
         const {password, ...others} = user._doc;
         
 //, accessToken
-        res.status(200).json({...others});
+        res.status(200).json(user);
 
     } catch(err) {
         // res.status(500).json(err);
